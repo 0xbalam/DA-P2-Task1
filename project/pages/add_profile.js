6d7844c8a3841022f77d70bfd5e6d5d1fcd6ab8a
0xBalam <0xb4l4m@gmail.com>
2022-07-26 02:52:56 -0700

    Disabling button

--- project/pages/add_profile.js
+++ project/pages/add_profile.js
@@ -1,8 +1,7 @@
 import Head from 'next/head'
-import Link from 'next/link'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 
 const AddProfile = ({ profileData, error }) => {
